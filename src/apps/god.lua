f037e9c0ff3aa832d1a7c91aedd45c31890649ed
Jamie Bray <jamie@jamiebray.xyz>
2021-05-15 17:45:15 +0800

    Fix god app

--- src/apps/god.lua
+++ src/apps/god.lua
@@ -1,4 +1,5 @@
 require("lib")
+require("src.common")
 
 local commands = require("src.world.commands")
 
@@ -11,7 +12,7 @@ while true do
 	local _, raw = rednet.receive("world")
 	c.report.info("received:", raw)
 	local msg = textutils.unserialiseJSON(raw)
-	local success, error = pcall(commands[msg.mode or "clone"], msg.size or 1)
+	local success, error = pcall(commands[msg.mode or "clone"], msg.size or 2)
 	if not success then
 		c.report.error(error)
 	end
