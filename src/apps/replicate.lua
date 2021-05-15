ce76fda242f4daca1bbcaffbe790d41d2b7e7f8a
Jamie Bray <jamie@jamiebray.xyz>
2021-05-15 18:09:19 +0800

    Add some assertions to the replicate script

--- src/apps/replicate.lua
+++ src/apps/replicate.lua
@@ -2,10 +2,10 @@ require("lib")
 require("src.common")
 
 local main = function()
-	c.state.reset()
+	assert(c.state.reset())
 
-	c.tree.chop({ first = true })
-	c.tree.harvestOnce()
+	assert(c.tree.chop({ first = true }))
+	assert(c.tree.harvestOnce())
 
 	while not c.goal.achieved(Array.concat(c.goal.GOALS.scan, c.goal.GOALS.mine)) do
 		-- Scan
@@ -23,27 +23,27 @@ local main = function()
 
 	-- Smelt required items
 	local furni = c.inventory.count(c.item.furnace)
-	c.craft.recipe(c.recipe[c.item.furnace], math.max(0, 3 - furni))
-	c.smelt.items({
+	assert(c.craft.recipe(c.recipe[c.item.furnace], math.max(0, 3 - furni)))
+	assert(c.smelt.items({
 		{ name = c.item.iron_ore, count = 14 },
 		{ name = c.item.cobblestone, count = 14 },
 		{ name = c.item.sand, count = 6 },
-	})
+	}))
 
 	-- Craft turtles
-	c.craft.recipe(c.recipe[c.item.glass_pane], 2)
-	c.craft.recipe(c.recipe[c.item.computer_normal], 2)
+	assert(c.craft.recipe(c.recipe[c.item.glass_pane], 2))
+	assert(c.craft.recipe(c.recipe[c.item.computer_normal], 2))
 	local planki = c.inventory.count(c.item.all.combustiblePlanks)
-	c.craft.recipe(c.recipe.plank, math.max(0, 42 - planki))
-	c.craft.recipe(c.recipe[c.item.chest], 3)
-	c.craft.recipe(c.recipe[c.item.turtle_normal], 2)
-	c.craft.recipe(c.recipe[c.item.stick], 5)
-	c.craft.recipe(c.recipe[c.item.diamond_pickaxe], 2)
-	c.craft.recipe(c.recipe[c.item.crafting_table], 2)
+	assert(c.craft.recipe(c.recipe.plank, math.max(0, 42 - planki)))
+	assert(c.craft.recipe(c.recipe[c.item.chest], 3))
+	assert(c.craft.recipe(c.recipe[c.item.turtle_normal], 2))
+	assert(c.craft.recipe(c.recipe[c.item.stick], 5))
+	assert(c.craft.recipe(c.recipe[c.item.diamond_pickaxe], 2))
+	assert(c.craft.recipe(c.recipe[c.item.crafting_table], 2))
 	-- Diamond picks don't stack, so you can't craft them together
-	c.craft.recipe(c.recipe.crafty_miney_turtle_normal, 1)
-	c.craft.recipe(c.recipe.crafty_miney_turtle_normal, 1)
-	c.craft.recipe(c.recipe[c.item.sign], 1)
+	assert(c.craft.recipe(c.recipe.crafty_miney_turtle_normal, 1))
+	assert(c.craft.recipe(c.recipe.crafty_miney_turtle_normal, 1))
+	assert(c.craft.recipe(c.recipe[c.item.sign], 1))
 
 	-- Go to chest location
 	assert(c.gps.goTo({ x = 0, y = 0, z = -4, r = 2 }))
@@ -57,13 +57,13 @@ local main = function()
 	assert(turtle.dropUp())
 
 	-- Place completion sign
-	c.move.forward({ times = 2 })
+	assert(c.move.forward({ times = 2 }))
 	c.move.up()
 	c.turn.around()
 	assert(c.inventory.select(c.item.all.signs))
 	assert(turtle.place("Complete"))
 	c.move.down()
-	c.move.forward({ times = 2 })
+	assert(c.move.forward({ times = 2 }))
 	c.turn.around()
 
 	-- Done
